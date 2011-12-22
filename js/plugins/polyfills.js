ba81358c6d9af27bb160fa37998a5c6d42eea51d
Ankur Oberoi <aoberoi@gmail.com>
2011-12-22 18:20:23 -0500

    adds window.requestAnimFrame to polyfills, refactors code into more-objecty approach

--- js/plugins/polyfills.js
+++ js/plugins/polyfills.js
@@ -1,3 +1,21 @@
+// see http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+
+// shim layer with setTimeout fallback
+
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       || 
+          window.webkitRequestAnimationFrame || 
+          window.mozRequestAnimationFrame    || 
+          window.oRequestAnimationFrame      || 
+          window.msRequestAnimationFrame     || 
+          function(callback, element){
+            window.setTimeout(function(){
+                callback(+new Date);
+            }, 1000 / 60);
+          };
+})();
+
+
 // ECMAScript5 Pollyfills for older browsers
 
 if (!Object.create) {
